9f34ca6f9b6aff42b36a6c3a75482ec25ee461d1
manuel <lic_alex_fer@yahoo.com.mx>
2016-12-22 15:25:36 -0600

    DGB
    Cambios de header y footer

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -1,3 +1,64 @@
+<main class="mdl-layout__content">
+            <div class="mdl-grid portfolio-max-width">
+
+            </div>
+            <style type="text/css">
+                .mdl-mega-footer__heading{
+                    color: black;
+                }
+            </style>
+           <footer class="mdl-mega-footer" style="background-color: rgb(255,193,7);color: black;">
+  <div class="mdl-mega-footer__middle-section">
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <img src="imagenes/vision2020.png">
+    </div>
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+      <h1 class="mdl-mega-footer__heading"><strong>Ubicación</strong></h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li>Av. Alfonso Reyes No. 4000 Nte. Col. Regina C.P. 64290</li>
+        <li>Monterrey, Nuevo León, México</li>
+        <li>Tel: +52 (81) 83.29.40.90</li>
+         <li>Fax: +52 (81) 83.29.40.65</li>
+
+      </ul>
+    </div>
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+      <h1 class="mdl-mega-footer__heading"><strong>Información relevante</strong></h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li><a href="#">Eventos</a></li>
+        <li><a href="#">Noticias</a></li>
+        <li><a href="#">Notas de interes</a></li>
+      </ul>
+    </div>
+
+    <div class="mdl-mega-footer__drop-down-section">
+      <input class="mdl-mega-footer__heading-checkbox" type="checkbox" checked>
+      <h1 class="mdl-mega-footer__heading"><strong>FAQ</strong></h1>
+      <ul class="mdl-mega-footer__link-list">
+        <li><a href="#">Preguntas frecuentes</a></li>
+        <li><a href="#">Mapa de sitio</a></li>
+        <li><a href="#">Suscríbete a Biblios</a></li>
+        <li><a href="#">Bolsa de trabajo</a></li>
+      </ul>
+    </div>
+
+  </div>
+
+  <div class="mdl-mega-footer__bottom-section">
+    <div class="mdl-logo" style="color: black;">© 2016 UANL
+Sitio desarrollado y mantenido por la Dirección de Bibliotecas
+de la Secretaría de Extensión y Cultura
+y el Sistema Integral de Bibliotecas de la UANL</div>
+  </div>
+
+</footer>
+        </main>
+    </div>
 <script src="https://code.getmdl.io/1.0.6/material.min.js"></script>
 </body>
 
